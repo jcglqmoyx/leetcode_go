f86a73bcaff265594630d45740b8a8e2f07bb8cc
Chintsai Hwo <jcglqmoyx@gmail.com>
2021-06-24 00:47:11 +0800

    update

--- 126.go
+++ 126.go
@@ -2,33 +2,60 @@ package main
 
 func findLadders(beginWord string, endWord string, wordList []string) [][]string {
 	n := len(beginWord)
-	set := make(map[string]bool)
-	dist := make(map[string]int)
+	set, dist := make(map[string]bool), make(map[string]int)
 	dist[beginWord] = 0
-	paths := [][]string{}
-	path := []string{}
+	var paths [][]string
+	path := []string{endWord}
 	for _, word := range wordList {
 		set[word] = true
 	}
-	var dfs = func(word string) {
+	var dfs = func(word string) {}
+	dfs = func(word string) {
 		if word == beginWord {
-			p := make([]string, n)
-			for i := 0; i < n; i++ {
-				p[i] = path[n-1-i]
+			p := make([]string, len(path))
+			for i := 0; i < len(path); i++ {
+				p[i] = path[len(path)-1-i]
 			}
-			paths = append(path, p)
+			paths = append(paths, p)
 		} else {
 			t := []byte(word)
 			for i := 0; i < n; i++ {
 				for c := 'a'; c <= 'z'; c++ {
+					tmp := t[i]
 					t[i] = byte(c)
 					s := string(t)
-					if set[s] && dist[s] == 0 {
-						dist[s] = dist[word] + 1
+					t[i] = tmp
+					if _, ok := dist[s]; ok && dist[s]+1 == dist[word] {
+						path = append(path, s)
+						dfs(s)
+						path = path[:len(path)-1]
 					}
 				}
 			}
 		}
 	}
+	q := []string{beginWord}
+	for len(q) > 0 {
+		t := q[0]
+		q = q[1:]
+		chars := []byte(t)
+		for i := 0; i < len(t); i++ {
+			for c := 'a'; c <= 'z'; c++ {
+				tmp := chars[i]
+				chars[i] = byte(c)
+				s := string(chars)
+				chars[i] = tmp
+				_, ok := dist[s]
+				if set[s] && !ok {
+					dist[s] = dist[t] + 1
+					if s == endWord {
+						break
+					}
+					q = append(q, s)
+				}
+			}
+		}
+	}
+	dfs(endWord)
 	return paths
 }
